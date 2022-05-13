81d60bf370f265ee4d6271bef9da6893193e5bff
xuexia_xu <xuexia_xu@sui.com>
2022-05-13 18:00:55 +0800

    first commit

---
+++ Hello/Hello.java
@@ -0,0 +1,13 @@
+//注释
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("Hello world! 你好");
+		System.out.println("北京\t天津\t上海");
+	}
+}
+
+class Dog{
+	public static void main(String[] args) {
+		System.out.println("Hello Dog!");
+	}
+}
